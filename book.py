4eb7074b778a68b885cb0d7e36ec6f1f0387fcca
Marcel Suter <marcel.suter@bzz.ch>
2024-08-20 09:31:40 +0200

    initial commit

--- book.py
+++ book.py
@@ -1,3 +1,7 @@
+"""
+Creates a book using dataclass and post_init
+"""
+
 from dataclasses import dataclass
 
 
@@ -5,13 +9,67 @@ from dataclasses import dataclass
 class Book:
     """
     A book in a library.
-
-    Attributes
-    ----------
+    """
     title: str
     author: str
-    available: bool
-    """
+    available: bool = True
+
+    def __post_init__(self):
+        if not self.title:
+            raise ValueError('title must not be empty')
+        if not self.author:
+            raise ValueError('author must not be empty')
+
+    @property
+    def title(self):
+        """
+        Gets the title.
+        :return:
+        """
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        """
+        Sets the title.
+        :param value:
+        :return:
+        """
+        self._title = value
+
+    @property
+    def author(self):
+        """
+        Gets the author.
+        :return:
+        """
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        """
+        Sets the author.
+        :param value:
+        :return:
+        """
+        self._author = value
+
+    @property
+    def available(self):
+        """
+        Gets the availability
+        :return:
+        """
+        return self._available
+
+    @available.setter
+    def available(self, value):
+        """
+        Sets the availability
+        :param value:
+        :return:
+        """
+        self._available = value
 
 
 if __name__ == '__main__':
